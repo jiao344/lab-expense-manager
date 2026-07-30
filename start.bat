652a0b33f11e3aba956f6a0caff03ecb91ddeaeb
jiao344 <76095133+jiao344@users.noreply.github.com>
2026-07-30 18:37:34 +0800

    添加 start.bat

---
+++ start.bat
@@ -0,0 +1,12 @@
+Exit code: 0
+Wall time: 0.4 seconds
+Output:
+@echo off
+chcp 65001 >nul
+title 鏈哄櫒浜虹爺绌舵墍鏃ュ父缁忚垂绠＄悊
+cd /d "%~dp0"
+python -m pip install -r requirements.txt
+start "" http://127.0.0.1:5000
+python app.py
+pause
+
