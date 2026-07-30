8d16cd88974436cbd8323b2fdf34cd3d0bfd14f8
jiao344 <76095133+jiao344@users.noreply.github.com>
2026-07-30 18:48:36 +0800

    修复 start.bat 的中文 UTF-8 编码

--- start.bat
+++ start.bat
@@ -1,9 +1,8 @@
 @echo off
 chcp 65001 >nul
-title 鏈哄櫒浜虹爺绌舵墍鏃ュ父缁忚垂绠＄悊
+title 机器人研究所日常经费管理
 cd /d "%~dp0"
 python -m pip install -r requirements.txt
 start "" http://127.0.0.1:5000
 python app.py
 pause
-
